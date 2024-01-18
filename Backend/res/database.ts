d5d3649d67c3e98ac41f9f8d8ce32d137ca89a1b
Nicolas Van-Duysen <nicolas.ssj.vdn@gmail.com>
2024-01-18 10:36:43 +0100

    feat(database): Création de la connexion à la base de données

---
+++ Backend/res/database.ts
@@ -0,0 +1,20 @@
+import mysql from "mysql2";
+
+export default function connectToDatabase(){
+    const connection = mysql.createConnection({
+        host: process.env.SQL_HOST,
+        user: process.env.SQL_USER,
+        password: process.env.SQL_PASSWORD,
+        database: process.env.SQL_DATABASE
+    });
+
+    connection.connect(function (err) {
+        if (!err) {
+            console.log("Database is connected");
+        } else {
+            console.log("Database is not connected" + err);
+        }
+    })
+
+    return connection;
+}
