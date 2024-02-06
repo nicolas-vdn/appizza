ed2c31319dddbe4365cc6c53c61c58f745240bcd
Nicolas Van-Duysen <nicolas.ssj.vdn@gmail.com>
2024-02-06 15:55:28 +0100

    feat (user) : Created User Entity

---
+++ Backend/src/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  salt: string;
+
+  @Column({ nullable: true })
+  authToken: string;
+}
