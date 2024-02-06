4e8bf6ca4e524a8f9af6d3a043ad65e75265327f
Nicolas Van-Duysen <nicolas.ssj.vdn@gmail.com>
2024-02-06 15:55:41 +0100

    feat (register) : Created Register Guard

---
+++ Backend/src/guards/register.guard.ts
@@ -0,0 +1,23 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class RegisterGuard implements CanActivate {
+  constructor(private userService: UserService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const body = context.switchToHttp().getRequest().body;
+    const user = await this.userService.findUser(body.username);
+
+    if (user) {
+      throw new UnauthorizedException('user already exists');
+    } else {
+      return true;
+    }
+  }
+}
