c78fafc14c3fb61ddc81dc7a130866aa56f3d57b
TristanRib <tristanrib_@outlook.fr>
2024-02-07 12:21:39 +0100

    feat(cart): cartPage + navigation

--- Frontend/lib/widgets/home_page.dart
+++ Frontend/lib/widgets/home_page.dart
@@ -21,56 +21,59 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const CartContent(),
-        Expanded(
-          child: Card(
-            margin: const EdgeInsets.only(right: 32, left: 32, bottom: 32),
-            color: Colors.white,
-            child: Consumer<CartProvider>(
-              builder: (context, cart, child) {
-                return ListView.separated(
-                  itemCount: pizzaList.length,
-                  itemBuilder: (context, index) {
-                    final pizza = pizzaList[index];
-                    return ListTile(
-                      shape: RoundedRectangleBorder(
-                        side: const BorderSide(color: Colors.black, width: 1),
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      title: Text(pizza.name),
-                      subtitle: Text('${pizza.price} €'),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          IconButton(
-                              onPressed: () => cart.removePizza(pizza),
-                              icon: const Icon(Icons.remove)),
-                          const SizedBox(width: 15),
-                          Text('${cart.quantityOf(pizza)}'),
-                          const SizedBox(width: 15),
-                          IconButton(
-                              onPressed: () => cart.addPizza(pizza),
-                              icon: const Icon(Icons.add)),
-                        ],
-                      ),
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return const SizedBox(height: 10);
-                  },
-                );
-              },
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          const CartContent(),
+          Expanded(
+            child: Card(
+              margin: const EdgeInsets.only(right: 32, left: 32, bottom: 32),
+              color: Colors.white,
+              child: Consumer<CartProvider>(
+                builder: (context, cart, child) {
+                  return ListView.separated(
+                    itemCount: pizzaList.length,
+                    itemBuilder: (context, index) {
+                      final pizza = pizzaList[index];
+                      return ListTile(
+                        shape: RoundedRectangleBorder(
+                          side: const BorderSide(color: Colors.black, width: 1),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        title: Text(pizza.name),
+                        subtitle: Text('${pizza.price} €'),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            IconButton(
+                                onPressed: () => cart.removePizza(pizza),
+                                icon: const Icon(Icons.remove)),
+                            const SizedBox(width: 15),
+                            Text('${cart.quantityOf(pizza)}'),
+                            const SizedBox(width: 15),
+                            IconButton(
+                                onPressed: () => cart.addPizza(pizza),
+                                icon: const Icon(Icons.add)),
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) {
+                      return const SizedBox(height: 10);
+                    },
+                  );
+                },
+              ),
             ),
           ),
-        ),
-        OutlinedButton.icon(
-          label: const Text('Valider la commande'),
-          onPressed: () => context.go('/cart'),
-          icon: const Icon(Icons.shopping_cart_checkout),
-        ),
-      ],
+          OutlinedButton.icon(
+            label: const Text('Valider la commande'),
+            onPressed: () => context.go('/cart'),
+            icon: const Icon(Icons.shopping_cart_checkout),
+          ),
+        ],
+      ),
     );
   }
 }
