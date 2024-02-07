3fd3fa77c6fc4a62fd23003a774e176585040e9a
ThomBsn <busin.thomas.tb@outlook.fr>
2024-02-07 21:26:03 +0100

    :lipstick: Small changes.

--- Frontend/lib/widgets/components/cart_content.dart
+++ Frontend/lib/widgets/components/cart_content.dart
@@ -1,5 +1,6 @@
 import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 import '../../providers/cart_provider.dart';
@@ -17,30 +18,58 @@ class CartContent extends StatelessWidget {
         color: const Color.fromARGB(255, 153, 0, 51),
         child: Consumer<CartProvider>(
           builder: (context, cart, child) {
-            return ExpandablePanel(
-              header: Padding(
-                padding: const EdgeInsets.all(32.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    const Center(
-                      child: Text(
-                        'Total',
-                        style: TextStyle(fontWeight: FontWeight.bold),
+            return Container(
+              // height: 200,
+              width: MediaQuery.of(context).size.width - 50,
+              child: ExpandablePanel(
+                header: Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Center(
+                        child: Row(
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Icon(
+                                Icons.shopping_cart,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              'Total : ${cart.total} €',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    const Center(
-                      child: Text(''),
-                    ),
-                    Center(
-                      child: Text('${cart.total} €'),
-                    ),
-                  ],
+                      OutlinedButton.icon(
+                        label: const Text(
+                          'Valider',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 204, 0, 0),
+                          ),
+                        ),
+                        onPressed: () => context.go('/cart'),
+                        icon: const Icon(
+                          Icons.arrow_forward,
+                          color: Color.fromARGB(255, 204, 0, 0),
+                        ),
+                        style: OutlinedButton.styleFrom(
+                          side: const BorderSide(width: 3.0, color: Color.fromARGB(255, 204, 0, 0)),
+                          padding: const EdgeInsets.all(8.0),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
+                collapsed: const SizedBox(),
+                expanded: cart.list.isEmpty ? const SizedBox() : const ExpandedRecap(),
               ),
-              collapsed: const SizedBox(),
-              expanded:
-                  cart.list.isEmpty ? const SizedBox() : const ExpandedRecap(),
             );
           },
         ),
@@ -79,18 +108,28 @@ class ExpandedRecap extends StatelessWidget {
                       children: [
                         Expanded(
                           child: Center(
-                              child: Text(
-                                  '${cart.list.values.toList()[index]} x')),
+                            child: Text(
+                              '${cart.list.values.toList()[index]} x',
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ),
                         ),
                         Expanded(
                           child: Center(
-                              child: Text(cart.list.keys.toList()[index].name)),
+                            child: Text(
+                              cart.list.keys.toList()[index].name,
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ),
                         ),
                         Expanded(
                           child: Center(
-                              child: Text(
-                                  '${cart.list.keys.toList()[index].price} €')),
-                        ),
+                            child: Text(
+                              '${cart.list.keys.toList()[index].price} €',
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        )
                       ],
                     ),
                   );
--- Frontend/lib/widgets/home_page.dart
+++ Frontend/lib/widgets/home_page.dart
@@ -36,147 +36,163 @@ class _HomePageState extends State<HomePage> {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          OutlinedButton.icon(
-            label: const Text('Valider la commande'),
-            onPressed: () => context.go('/cart'),
-            icon: const Icon(Icons.shopping_cart_checkout),
-          ),
-          Expanded(
-            child: Consumer<CartProvider>(
-              builder: (context, cart, child) {
-                return CarouselSlider(
-                  options: CarouselOptions(height: 500.0),
-                  items: _pizzaList.map((pizza) {
-                    return Builder(
-                      builder: (BuildContext context) {
-                        return Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            Stack(
-                              clipBehavior: Clip.none,
-                              alignment: AlignmentDirectional.topCenter,
-                              children: [
-                                Positioned(
-                                  child: Container(
-                                    width: MediaQuery.of(context).size.width,
-                                    margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                                    decoration: const BoxDecoration(
-                                      gradient: LinearGradient(
-                                        begin: Alignment.topRight,
-                                        end: Alignment.bottomLeft,
-                                        stops: [0.1, 0.9],
-                                        colors: [
-                                          Color.fromARGB(255, 204, 0, 0),
-                                          Color.fromARGB(255, 153, 0, 51),
-                                        ],
-                                      ),
-                                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Colors.grey,
-                                          offset: Offset(0.0, 2.0),
-                                          blurRadius: 6.0,
+          Stack(
+            alignment: AlignmentDirectional.bottomCenter,
+            clipBehavior: Clip.none,
+            children: [
+              Column(
+                children: [
+                  Consumer<CartProvider>(
+                    builder: (context, cart, child) {
+                      return CarouselSlider(
+                        options: CarouselOptions(
+                          height: 500.0,
+                          enlargeCenterPage: true,
+                        ),
+                        items: _pizzaList.map((pizza) {
+                          return Builder(
+                            builder: (BuildContext context) {
+                              return Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: [
+                                  Stack(
+                                    clipBehavior: Clip.none,
+                                    alignment: AlignmentDirectional.topCenter,
+                                    children: [
+                                      Positioned(
+                                        child: Container(
+                                          width: MediaQuery.of(context).size.width,
+                                          margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                                          decoration: const BoxDecoration(
+                                            gradient: LinearGradient(
+                                              begin: Alignment.topRight,
+                                              end: Alignment.bottomLeft,
+                                              stops: [0.1, 0.9],
+                                              colors: [
+                                                Color.fromARGB(255, 204, 0, 0),
+                                                Color.fromARGB(255, 153, 0, 51),
+                                              ],
+                                            ),
+                                            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                                            boxShadow: [
+                                              BoxShadow(
+                                                color: Colors.grey,
+                                                offset: Offset(0.0, 2.0),
+                                                blurRadius: 6.0,
+                                              ),
+                                            ],
+                                          ),
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(left: 32.0, top: 128, right: 32, bottom: 32),
+                                            child: Column(
+                                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                              children: [
+                                                Row(
+                                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                  children: [
+                                                    Column(
+                                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                                      children: [
+                                                        Text(
+                                                          pizza.name,
+                                                          style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                                                          softWrap: true,
+                                                        ),
+                                                        Text(
+                                                          "${pizza.price} €",
+                                                          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                    const Card(
+                                                      color: Colors.transparent,
+                                                      child: Padding(
+                                                        padding: EdgeInsets.all(8.0),
+                                                        child: Row(
+                                                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                                          children: [Text("4.3"), Icon(Icons.star)],
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                                const SizedBox(
+                                                  height: 32,
+                                                ),
+                                                Row(
+                                                  mainAxisAlignment: MainAxisAlignment.end,
+                                                  children: [
+                                                    ElevatedButton(
+                                                      onPressed: () => cart.removePizza(pizza),
+                                                      style: ElevatedButton.styleFrom(
+                                                        backgroundColor: Colors.white,
+                                                        padding: const EdgeInsets.all(20),
+                                                        shape: const CircleBorder(),
+                                                      ),
+                                                      child: const Icon(Icons.remove),
+                                                    ),
+                                                    Padding(
+                                                      padding: const EdgeInsets.all(8.0),
+                                                      child: Text(
+                                                        '${cart.quantityOf(pizza)}',
+                                                        style: const TextStyle(fontSize: 25),
+                                                      ),
+                                                    ),
+                                                    ElevatedButton(
+                                                      onPressed: () => cart.addPizza(pizza),
+                                                      style: ElevatedButton.styleFrom(
+                                                        backgroundColor: Colors.white,
+                                                        padding: const EdgeInsets.all(20),
+                                                        shape: const CircleBorder(),
+                                                      ),
+                                                      child: const Icon(Icons.add),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ],
+                                            ),
+                                          ),
                                         ),
-                                      ],
-                                    ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.only(left: 32.0, top: 128, right: 32, bottom: 32),
-                                      child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Row(
-                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                            children: [
-                                              Column(
-                                                crossAxisAlignment: CrossAxisAlignment.start,
-                                                children: [
-                                                  Text(
-                                                    pizza.name,
-                                                    style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                                                    softWrap: true,
-                                                  ),
-                                                  Text(
-                                                    "${pizza.price} €",
-                                                    style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                                                  ),
-                                                ],
-                                              ),
-                                              const Card(
-                                                color: Colors.transparent,
-                                                child: Padding(
-                                                  padding: EdgeInsets.all(8.0),
-                                                  child: Row(
-                                                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                                    children: [Text("4.3"), Icon(Icons.star)],
-                                                  ),
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                          const SizedBox(
-                                            height: 32,
-                                          ),
-                                          Row(
-                                            mainAxisAlignment: MainAxisAlignment.end,
-                                            children: [
-                                              ElevatedButton(
-                                                onPressed: () => cart.removePizza(pizza),
-                                                style: ElevatedButton.styleFrom(
-                                                  backgroundColor: Colors.white,
-                                                  padding: const EdgeInsets.all(20),
-                                                  shape: const CircleBorder(),
-                                                ),
-                                                child: const Icon(Icons.remove),
-                                              ),
-                                              Padding(
-                                                padding: const EdgeInsets.all(8.0),
-                                                child: Text('${cart.quantityOf(pizza)}'),
-                                              ),
-                                              ElevatedButton(
-                                                onPressed: () => cart.addPizza(pizza),
-                                                style: ElevatedButton.styleFrom(
-                                                  backgroundColor: Colors.white,
-                                                  padding: const EdgeInsets.all(20),
-                                                  shape: const CircleBorder(),
-                                                ),
-                                                child: const Icon(Icons.add),
-                                              ),
-                                            ],
-                                          ),
-                                        ],
                                       ),
-                                    ),
+                                      Positioned(
+                                        top: -100,
+                                        child: SizedBox(
+                                          width: 200,
+                                          height: 200,
+                                          child: ClipRRect(
+                                            borderRadius: BorderRadius.circular(20.0), // Ajustez la valeur du border radius selon vos besoins
+                                            child: Image.network(
+                                              pizza.url, // Remplacez l'URL par l'URL de votre image
+                                              width: 200.0, // Ajustez la largeur de l'image selon vos besoins
+                                              height: 200.0, // Ajustez la hauteur de l'image selon vos besoins
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                    ],
                                   ),
-                                ),
-                                Positioned(
-                                  top: -100,
-                                  child: SizedBox(
-                                    width: 200,
-                                    height: 200,
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(20.0), // Ajustez la valeur du border radius selon vos besoins
-                                      child: Image.network(
-                                        pizza.url, // Remplacez l'URL par l'URL de votre image
-                                        width: 200.0, // Ajustez la largeur de l'image selon vos besoins
-                                        height: 200.0, // Ajustez la hauteur de l'image selon vos besoins
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  }).toList(),
-                );
-              },
-            ),
+                                ],
+                              );
+                            },
+                          );
+                        }).toList(),
+                      );
+                    },
+                  ),
+                  const SizedBox(
+                    height: 150,
+                  ),
+                ],
+              ),
+              const Positioned(
+                bottom: 0,
+                child: CartContent(),
+              ),
+            ],
           ),
-          const CartContent(),
         ],
       ),
     );
