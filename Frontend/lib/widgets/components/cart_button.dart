888d83b3a2084b3c8f689fa1fa90f959a5cb3be1
TristanRib <tristanrib_@outlook.fr>
2024-02-16 15:39:23 +0100

    fix(cartContent): clean

--- Frontend/lib/widgets/components/cart_button.dart
+++ Frontend/lib/widgets/components/cart_button.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/widgets/components/cart_content.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
@@ -24,56 +23,92 @@ class CartButton extends StatelessWidget {
             ),
             enableFeedback: cart.totalQuantity() > 0,
             onPressed: () {
-              cart.totalQuantity() > 0
-                  ? showModalBottomSheet(
-                      context: context,
-                      builder: (context) => Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const CartContent(),
-                              DecoratedBox(
-                                decoration: BoxDecoration(
-                                  gradient: const LinearGradient(colors: [
-                                    Color.fromARGB(255, 204, 0, 0),
-                                    Color.fromARGB(255, 153, 0, 51),
-                                  ]),
-                                  borderRadius: BorderRadius.circular(5),
-                                ),
-                                child: ElevatedButton.icon(
-                                  style: ElevatedButton.styleFrom(
-                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-                                    padding: const EdgeInsets.all(16.0),
-                                    backgroundColor: Colors.transparent,
-                                    foregroundColor: Colors.transparent,
-                                    shadowColor: Colors.transparent,
-                                  ),
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                    context.go("/cart");
-                                  },
-                                  icon: const Icon(
-                                    Icons.checklist,
-                                    color: Colors.white,
-                                  ),
-                                  label: const Text(
-                                    "Continuer",
-                                    style: TextStyle(color: Colors.white, fontSize: 20),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    )
-                  : null;
+              cart.totalQuantity() > 0 ? buildShowModalBottomSheet(context) : null;
             },
           ),
         ],
       );
     });
+  }
+
+  Future<dynamic> buildShowModalBottomSheet(BuildContext context) {
+    return showModalBottomSheet(
+      context: context,
+      builder: (context) => Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                constraints: const BoxConstraints(maxHeight: 300),
+                child: Consumer<CartProvider>(builder: (context, cart, child) {
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: cart.list.length,
+                    itemBuilder: (context, index) {
+                      final item = cart.list.entries.toList()[index];
+
+                      return ListTile(
+                        leading: Text("${item.value} x",
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            )),
+                        title: Center(
+                          child: Text(item.key.name,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              )),
+                        ),
+                        trailing: Text("${item.key.price} €",
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            )),
+                      );
+                    },
+                  );
+                }),
+              ),
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(colors: [
+                    Color.fromARGB(255, 204, 0, 0),
+                    Color.fromARGB(255, 153, 0, 51),
+                  ]),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+                    padding: const EdgeInsets.all(16.0),
+                    backgroundColor: Colors.transparent,
+                    foregroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    context.go("/cart");
+                  },
+                  icon: const Icon(
+                    Icons.checklist,
+                    color: Colors.white,
+                  ),
+                  label: const Text(
+                    "Continuer",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
